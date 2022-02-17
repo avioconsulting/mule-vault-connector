d55ac8b275fcd6b2a155b1711e414f91c2e66a4f
Talysson de Castro <talyssoncastro@gmail.com>
2022-02-17 17:48:43 -0300

    Implement tests for AwsV4SignProperties using data driver approach

---
+++ src/test/java/com/avioconsulting/mule/connector/vault/provider/internal/vault/client/auth/AWSV4SignPropertiesTests.java
@@ -0,0 +1,58 @@
+package com.avioconsulting.mule.connector.vault.provider.internal.vault.client.auth;
+
+import com.avioconsulting.mule.connector.vault.provider.internal.vault.client.auth.algorithm.AWSV4SignProperties;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+@RunWith(Parameterized.class)
+public class AWSV4SignPropertiesTests {
+
+    private final String url;
+    private final String host;
+    private final String canonicalUri;
+    private final Map<String, String> queryParameters;
+
+    @Parameterized.Parameters(name = "{index}: Test with url={0}, host: {1}, canonicalUri: {2}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"http://sts.amazonaws.com", "sts.amazonaws.com", "", Collections.emptyMap()},
+                {"https://sts.amazonaws.com", "sts.amazonaws.com", "", Collections.emptyMap()},
+                {"https://sts.amazonaws.com/", "sts.amazonaws.com", "/", Collections.emptyMap()},
+                {"https://sts.amazonaws.com/?", "sts.amazonaws.com", "/", Collections.emptyMap()},
+                {"https://sts.amazonaws.com/?Action", "sts.amazonaws.com", "/", Collections.emptyMap()},
+                {"https://sts.amazonaws.com/?Action=", "sts.amazonaws.com", "/", Collections.emptyMap()},
+                {"https://sts.amazonaws.com/?Action=GetCallerIdentity", "sts.amazonaws.com", "/", Collections.singletonMap("Action", "GetCallerIdentity")},
+                {"https://sts.amazonaws.com/?Action=Your%20text", "sts.amazonaws.com", "/", Collections.singletonMap("Action", "Your text")},
+                {"https://sts.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15", "sts.amazonaws.com", "/", ImmutableMap.of("Action", "GetCallerIdentity", "Version", "2011-06-15")},
+                {"https://sts.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15", "sts.amazonaws.com", "/", ImmutableMap.of("Action", "GetCallerIdentity", "Version", "2011-06-15")},
+                {"https://sts.amazonaws.com?Action=GetCallerIdentity&Version=2011-06-15", "sts.amazonaws.com", "", ImmutableMap.of("Action", "GetCallerIdentity", "Version", "2011-06-15")},
+                {":invalid-url.com", "sts.amazonaws.com", "/", Collections.emptyMap()},
+        });
+    }
+
+    public AWSV4SignPropertiesTests(String url, String host, String canonicalUri, Map<String, String> queryParameters) {
+        this.url = url;
+        this.host = host;
+        this.canonicalUri = canonicalUri;
+        this.queryParameters = queryParameters;
+    }
+
+    @Test
+    public void testUrl() {
+
+        AWSV4SignProperties awsV4SignProperties = new AWSV4SignProperties(url);
+
+        Assert.assertEquals(host, awsV4SignProperties.getHost());
+        Assert.assertEquals(canonicalUri, awsV4SignProperties.getCanonicalUri());
+        Assert.assertEquals(queryParameters, awsV4SignProperties.getQueryParameters());
+
+    }
+
+}
